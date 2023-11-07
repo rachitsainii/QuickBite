a9a184ad44afd2a4356b306d2773a89c85a885ae
rachitsainii <rachit.saini@icloud.com>
2023-11-07 13:27:26 -0800

    Added react navigation

---
+++ final/screens/AboutScreen.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function AboutScreen() {
+    return (
+        <SafeAreaView>
+            <Text>AboutScreen</Text>
+        </SafeAreaView>
+    )
+}
